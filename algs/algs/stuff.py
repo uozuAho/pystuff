9fc3c9c65200c5ded2c403d52c46812a112f0981
me <me@me.com>
2025-11-10 17:44:57 +1100

    add algs

---
+++ algs/algs/stuff.py
@@ -0,0 +1,2 @@
+def hello_msg(name):
+    return f"hi {name}!"
---
+++ algs/algs_tests/stuff_test.py
@@ -0,0 +1,9 @@
+from algs.stuff import hello_msg
+
+
+def test_hello_msg():
+    assert hello_msg("bert") == "hi bert!"
+
+
+def test_hello_msg_b(benchmark):
+    benchmark(hello_msg, "aaaaaaaa")
