eaa6dad0a63fd67b12809e1236c6067513520072
me <me@me.com>
2025-12-03 12:00:09 +1100

    2015 17

---
+++ aoc/2015/17.py
@@ -0,0 +1,35 @@
+import itertools
+import sys
+from aocd import get_data, submit
+
+samp = """20
+15
+10
+5
+5"""
+
+# 10m
+def solve(input: str):
+    nums = [int(x) for x in input.splitlines()]
+    combos = []
+    ccount = 0
+    minlen = 99999
+    for i in range(1, len(nums) + 1):
+        for c in itertools.combinations(nums, i):
+            if sum(c) == 150:
+                combos.append(c)
+                ccount += 1
+                if len(c) > 0 and len(c) < minlen:
+                    minlen = len(c)
+    return sum(1 if len(x) == minlen else 0 for x in combos)
+
+if __name__ == "__main__":
+    year, day = 2015, 17
+    real = get_data(year=year, day=day)
+
+    if 'print' in sys.argv:
+        print(solve(samp))
+    elif 'submit' in sys.argv:
+        submit(solve(real), year=year, day=day)
+    else:
+        solve(samp)
