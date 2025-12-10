2adb5db63ba339c15e43e713b055e6984be3292f
me <me@me.com>
2025-12-10 15:34:24 +1100

    WIP: pipe utils

--- aoc/utils/basic.py
+++ aoc/utils/basic.py
@@ -4,3 +4,16 @@ type Pred = t.Callable[[t.Any], bool]
 
 def count(seq, pred: Pred):
     return sum(1 if pred(x) else 0 for x in seq)
+
+
+def pipe(init, *funcs):
+    """ pipe functions together, like F#'s |>
+        Eg. instead of d(c(b(a()))):
+        pipe(a, b, c, d)
+
+        Works with generators, dunno how!
+    """
+    current = init
+    for f in funcs:
+        current = f(current)
+    return current
