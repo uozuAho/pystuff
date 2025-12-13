17057eaf6b93e97f00abef7abae4b42b545b83b7
me <me@me.com>
2025-12-13 17:37:17 +1100

    tmp starting point

--- aoc/tmp.py
+++ aoc/tmp.py
@@ -1,67 +1,24 @@
-from collections import Counter
-import utils.grid as g
-from pipe import map
-from utils.input import lines, transpose
+from pipe import tee
+from utils.input import lines
+from utils.pipe import ignore
 
 test = """
-eedadn
-drvtee
-eandsr
-raavrd
-atevrs
-tsrnev
-sdttsa
-rasrtv
-nssdts
-ntnada
-svetve
-tesnvt
-vntsnd
-vrdear
-dvrsen
-enarar
+abc
+def
 """
 
+real = """
+"""
 
-# same but way more elegant: https://github.com/iKevinY/advent/blob/main/2016/day06.py
+list(
+lines(test)
+| tee
+) # type: ignore
 
 
 def solve(input: str):
-    grid = g.parse_chargrid(input)
-    c = [Counter(col) for col in g.cols(grid)]
-    letters = [x.most_common(1) for x in c]
-    return "".join(x[0][0] for x in letters)
-
-
-def solve2(input: str):
-    grid = g.parse_chargrid(input)
-    c = [Counter(col) for col in g.cols(grid)]
-    letters = [x.most_common()[-1] for x in c]
-    return "".join(x[0][0] for x in letters)
-
-
-def pipesolve1(input):
-    # fmt: off
-    return "".join(
-        transpose(lines(input))
-        | map(Counter)
-        | map(lambda x: x.most_common()[0][0])
-    )
-    # fmt: on
-
-
-def pipesolve2(input):
-    # fmt: off
-    return "".join(
-        transpose(lines(input))
-        | map(Counter)
-        | map(lambda x: x.most_common()[-1][0])
-    )
-    # fmt: on
+    return ""
 
 
 def test_sols():
-    assert solve(test) == "easter"
-    assert solve2(test) == "advent"
-    assert pipesolve1(test) == "easter"
-    assert pipesolve2(test) == "advent"
+    assert solve(test) == ""
